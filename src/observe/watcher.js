9b6ff1d4c71585c52ef171d6a9c58d5bea206689
zdshen <sleepblacksheep@icloud.com>
2024-01-21 15:45:48 +0800

    列队

--- src/observe/watcher.js
+++ src/observe/watcher.js
@@ -8,7 +8,7 @@ class watcher {
     this.cb = cb
     this.options = options
     this.id = id++
-    this.deps = []  // watcher 存放 dep
+    this.deps = [] // watcher 存放 dep
     this.depsId = new Set()
 
     // 判断
@@ -18,14 +18,18 @@ class watcher {
     this.get()
   }
 
-  addDep(dep){
+  addDep(dep) {
     // 去重
     let id = dep.id
-    if(!this.depsId.has(id)){
+    if (!this.depsId.has(id)) {
       this.deps.push(dep)
       this.depsId.add(id)
       dep.addSub(this)
     }
+  }
+
+  run() {
+    this.get()
   }
 
   // 初次渲染
@@ -37,7 +41,31 @@ class watcher {
 
   // 更新
   update() {
-    this.getter()
+    // 数据更新后会调用 get 方法，所以需要缓存更新数据
+    // this.get()
+    queueWatcher(this)
+  }
+}
+
+let queue = []
+let has = {}
+let pending = false
+function queueWatcher(watcher) {
+  let id = watcher.id // 每个组件都是同一个id
+  // 防抖 == 和 === 有区别
+  if (has[id] == null) {
+    queue.push(watcher)
+    has[id] = true
+    if (!pending) {
+      // 异步
+      setTimeout(() => {
+        queue.forEach(item => item.run())
+        queue = []
+        has = {}
+        pending = false
+      }, 0)
+    }
+    pending = true
   }
 }
 
